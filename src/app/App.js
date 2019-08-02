241c96ba5a78c67192435b2b3ecf4bbf6763e98e
Tom Dugan <thomas@aadwebb.com>
2019-08-02 11:20:06 -0500

    Try to transmit the CGI firewall, maybe it will index the digital network

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EmailInput from '../components/email-input';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      email: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <EmailInput
+          value={this.state.email}
+          placeholder="Your email address"
+          handleChange={this.handleChange}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
